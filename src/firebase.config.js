8f19ea5c698413d7e5857ecc50c68bf8f3baab58
Bhowmik-puja <bhowmikpuja27@gmail.com>
2020-12-07 19:21:32 +0600

    final commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQP60bus04Synt7Tk7Yyl2W533z0rqZc0",
+    authDomain: "ema-john-a7470.firebaseapp.com",
+    databaseURL: "https://ema-john-a7470.firebaseio.com",
+    projectId: "ema-john-a7470",
+    storageBucket: "ema-john-a7470.appspot.com",
+    messagingSenderId: "487430316083",
+    appId: "1:487430316083:web:e1b72f407eae9609b6c346"
+  };
+  export default firebaseConfig;
